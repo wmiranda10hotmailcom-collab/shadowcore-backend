232cf4f5544e16c11bed81b14d22c81e4f3d1a82
wmiranda10hotmailcom-collab <wmiranda10hotmail.com@gmail.com>
2026-05-11 00:45:55 -0300

    Add new allowed origin to CORS configuration

--- utils/cors.js
+++ utils/cors.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   'https://shadowcoreapp.com',
+  'https://shadowcore.lovable.app',
   'https://rota-lucro-diario-axm5e9iug.vercel.app',
   'https://preview-shadowcore.lovable.app',
   'http://localhost:5173'
@@ -7,13 +8,17 @@ const allowedOrigins = [
 
 function handleCors(req, res) {
   const origin = req.headers.origin;
-  
+
   if (allowedOrigins.includes(origin) || !origin) {
     res.setHeader('Access-Control-Allow-Origin', origin || '*');
   }
 
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
@@ -23,6 +28,7 @@ function handleCors(req, res) {
     res.status(200).end();
     return true;
   }
+
   return false;
 }
 
